c139c7c7456a5671124e81e40274e67a19618285
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-02 08:32:04 +0100

    Day 02 (C)

---
+++ day02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int remap(int i,int skip){return i>=skip?i+1:i;}
+
+int main(void){
+    int part1=0, part2=0; char line[100]; int L[10];
+    FILE *f=fopen("data/input02.txt","r");
+    while(fgets(line,100,f)){
+        int N=sscanf(line,"%d %d %d %d %d %d %d %d",L,L+1,L+2,L+3,L+4,L+5,L+6,L+7);
+        int ok(int skip){
+            for(int i=0; i<(skip==999?N-1:N-2); i++){
+                int e1=L[remap(1,skip)]-L[remap(0,skip)];
+                int e2=L[remap(i+1,skip)]-L[remap(i,skip)];
+                if(e1*e2<=0 || e2*e2>9){ return 0; }
+            }
+            return 1;
+        }
+        part1+=ok(999);
+        for(int skip=0; skip<N; skip++){
+            if(ok(skip)){part2++; break;}
+        }
+    }
+    printf("%d %d\n",part1,part2);
+}
