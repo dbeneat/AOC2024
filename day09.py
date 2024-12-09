4a7516a31109128ba242942676a9ca2b73979308
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-09 18:23:03 +0100

    Faster implementation, runs in 0.15s

--- day09.py
+++ day09.py
@@ -1,18 +1,30 @@
+from heapq import *
+from time import perf_counter
+tic=perf_counter()
 with open("data/input09.txt") as f:
     inp=f.read().strip()
 
 def arith(start,nb):
     return (start+start+nb-1)*nb//2
 
+def firstBeforePos(voids,pos,sz):
+    ibest=None
+    best=pos
+    for i in range(sz,10):
+        if voids[i] and voids[i][0][0]<best:
+            ibest=i
+            best=voids[i][0][0]
+    return ibest
+
 def solve(inp,ispart2):
     inp+="9"
     res=0
     pos=0
-    voids,blocks=[],[]
+    voids,blocks=[[] for k in range(10)],[]
     for i,x in enumerate(inp.strip()):
         length=int(x)
         if i%2==0: blocks.append([pos,length])
-        else: voids.append([pos,length])
+        else: heappush(voids[length],(pos,length))
         pos+=length
     numbl=len(blocks)-1
     freebl=0
@@ -22,15 +34,12 @@ def solve(inp,ispart2):
             nb=1
             if ispart2:
                 nb=sz
-                freebl=0
-                while voids[freebl][1]<sz: freebl+=1
-            else:
-                while voids[freebl][1]==0: freebl+=1
+            i=firstBeforePos(voids,pos,nb)           
             blocks[numbl][1]=sz-nb
-            if freebl<numbl:
-                res+=arith(voids[freebl][0],nb)*numbl
-                voids[freebl][0]+=nb
-                voids[freebl][1]-=nb
+            if i:
+                freebl,blsz=heappop(voids[i])
+                heappush(voids[blsz-nb],(freebl+nb,blsz-nb))
+                res+=arith(freebl,nb)*numbl
             else:
                 res+=arith(pos+sz-nb,nb)*numbl
         else:
