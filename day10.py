bcf7dffcd880f9283bc67f63212b1e1016ade6aa
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-10 07:18:05 +0100

    Day 10 (Python)

--- day10.py
+++ day10.py
@@ -4,13 +4,10 @@ H,W=len(L),len(L[0])
 
 def grade(x,y,seen,memo):
     seen.add((x,y))
-    if L[y][x]==9:
-        memo[(x,y)]=1
-        return 1,1
+    if L[y][x]==9: memo[(x,y)]=1; return 1,1
     score,rating=0,0
     for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-        if x+dx<0 or x+dx>=W or y+dy<0 or y+dy>=H:
-            continue
+        if x+dx<0 or x+dx>=W or y+dy<0 or y+dy>=H: continue
         if L[y+dy][x+dx]==L[y][x]+1:
             notseen=(x+dx,y+dy) not in seen
             sc,r=grade(x+dx,y+dy,seen,memo)
