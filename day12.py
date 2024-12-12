dc39fa54828835c93d7611eef4c9658e0952cee1
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-12 07:32:13 +0100

    Shorter code

--- day12.py
+++ day12.py
@@ -2,17 +2,14 @@ from collections import defaultdict
 with open("data/input12.txt") as f:
     L=[list(x) for x in f.read().split()]
 W,H=len(L),len(L[0])
-area=defaultdict(int)
-perim=defaultdict(int)
-corners=defaultdict(int)
+area,perim,corners=defaultdict(int),defaultdict(int),defaultdict(int)
 
 CC=[[0]*W for y in range(len(L))]
 seen={}
 numcc=0
 def dfs(x,y):
     global numcc
-    if (x,y) in seen:
-        return
+    if (x,y) in seen: return
     seen[(x,y)]=True
     CC[y][x]=numcc
     if x>0 and L[y][x-1]==L[y][x]: dfs(x-1,y)
@@ -23,28 +20,24 @@ def dfs(x,y):
 for y in range(H):
     for x in range(W):
         if (x,y) not in seen:
-            dfs(x,y)
-            numcc+=1
+            dfs(x,y); numcc+=1
 
 def get(x,y):
     global W,H
-    if x<0 or x>=W or y<0 or y>=H:
-        return -1
+    if x<0 or x>=W or y<0 or y>=H: return -1
     return CC[y][x]
 
 for y in range(H):
     for x in range(W):
-        if (get(x-1,y)!=get(x,y) and get(x,y-1)!=get(x,y)) or (get(x-1,y)==get(x,y) and get(x,y-1)==get(x,y) and get(x,y)!=get(x-1,y-1)): corners[get(x,y)]+=1
-        if (get(x-1,y)!=get(x,y) and get(x,y+1)!=get(x,y)) or (get(x-1,y)==get(x,y) and get(x,y+1)==get(x,y) and get(x,y)!=get(x-1,y+1)): corners[get(x,y)]+=1
-        if (get(x+1,y)!=get(x,y) and get(x,y-1)!=get(x,y)) or (get(x+1,y)==get(x,y) and get(x,y-1)==get(x,y) and get(x,y)!=get(x+1,y-1)): corners[get(x,y)]+=1
-        if (get(x+1,y)!=get(x,y) and get(x,y+1)!=get(x,y)) or (get(x+1,y)==get(x,y) and get(x,y+1)==get(x,y) and get(x,y)!=get(x+1,y+1)): corners[get(x,y)]+=1
+        for dx,dy in ((-1,-1),(-1,1),(1,-1),(1,1)):
+            a,b,c,d=get(x,y),get(x+dx,y),get(x,y+dy),get(x+dx,y+dy)
+            if (a!=b and a!=c) or (a==b and a==c and a!=d): corners[a]+=1
         k=4
-        if x>0 and CC[y][x-1]==CC[y][x]:k-=2
-        if y>0 and CC[y-1][x]==CC[y][x]:k-=2
-        area[CC[y][x]]+=1
-        perim[CC[y][x]]+=k
+        a,b,c=get(x,y),get(x-1,y),get(x,y-1)
+        if a==b:k-=2
+        if a==c:k-=2
+        area[a]+=1; perim[a]+=k
 
-         
 part1=sum(area[x]*perim[x] for x in area)
 part2=sum(area[x]*corners[x] for x in area)
 print(part1,part2)
