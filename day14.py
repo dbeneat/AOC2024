154ed96fc6f5b823a4e84c68e0fcff1d93226dbc
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-14 12:09:47 +0100

    Almost 80x faster with CRT

--- day14.py
+++ day14.py
@@ -9,28 +9,28 @@ def update(robot,steps):
     robot[0]=(x+steps*vx)%W
     robot[1]=(y+steps*vy)%H
 
-for r in robots:
-    update(r,100)
+for i in range(100):
+    LH,LV=[0]*W,[0]*H
+    Hok,Vok=False,False
+    for r in robots:
+        if LH[r[0]]>20:
+            Hok=True
+        else:
+            LH[r[0]]+=1
+        if LV[r[1]]>20:
+            Vok=True
+        else:
+            LV[r[1]]+=1
+        update(r,1)
+    if Hok:Hresidue=i
+    if Vok:Vresidue=i
+    
 a=len([r for r in robots if r[0]<(W-1)//2 and r[1]<(H-1)//2])
 b=len([r for r in robots if r[0]<(W-1)//2 and r[1]>(H-1)//2])
 c=len([r for r in robots if r[0]>(W-1)//2 and r[1]<(H-1)//2])
 d=len([r for r in robots if r[0]>(W-1)//2 and r[1]>(H-1)//2])
 part1=a*b*c*d
-
-for t in range(100,10000):
-    buffer=[[" "]*W for i in range(H)]
-    for r in robots:
-        x,y,_,_=r
-        buffer[y][x]="*"
-        update(r,1)
-    c=False
-    for y in range(H):
-        c=c or "*********" in "".join(buffer[y])
-    if c:
-        print("time=",t)
-        for line in buffer:
-            print("".join(line))
-        part2=t
-        break
-
+for k in range(W*H):
+    if k%W==Hresidue and k%H==Vresidue:
+        part2=k;break
 print(part1,part2)
