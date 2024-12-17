1a8fb5deb230c08b9374e7e2782c96a3d389ac0f
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-17 16:42:47 +0100

    Shorter code

--- day17.py
+++ day17.py
@@ -5,56 +5,31 @@ reg=[int(reg[2]),int(reg[5]),int(reg[8])]
 prog=[int(k) for k in prog[9:].split(",")]
 
 def combo(n,reg):
-    if 0<=n<=3:
-        return n
-    if 4<=n<=6:
-        return reg[n-4]
-    if n==7:
-        print("erreur")
+    if 0<=n<=3:return n
+    if 4<=n<=6:return reg[n-4]
+    if n==7:print("erreur")
 
 def execute(prog,reg):
     output=[]
     ip=0
     while ip<len(prog):
-        #print("...reg", reg)
         op1,op2=prog[ip],prog[ip+1]
         comb=combo(op2,reg)
-        if op1==0:
-            num,denom=reg[0],2**comb
-            reg[0]=num//denom
-            ip+=2
-        if op1==1:
-            reg[1]=op2^reg[1]
-            ip+=2
-        if op1==2:
-            reg[1]=comb%8
-            ip+=2
-        if op1==3:
-            if reg[0]!=0:
-                ip=op2
-            else:
-                ip+=2
-        if op1==4:
-            reg[1]=reg[1]^reg[2]
-            ip+=2
-        if op1==5:
-            output.append(comb%8)
-            ip+=2
-        if op1==6:
-            num,denom=reg[0],2**comb
-            reg[1]=num//denom
-            ip+=2
-        if op1==7:
-            num,denom=reg[0],2**comb
-            reg[2]=num//denom
-            ip+=2
+        if op1==0: num,denom=reg[0],2**comb;reg[0]=num//denom
+        if op1==1: reg[1]=op2^reg[1]
+        if op1==2: reg[1]=comb%8
+        if op1==3 and reg[0]!=0: ip=op2; continue
+        if op1==4: reg[1]=reg[1]^reg[2]
+        if op1==5: output.append(comb%8)
+        if op1==6: num,denom=reg[0],2**comb;reg[1]=num//denom
+        if op1==7: num,denom=reg[0],2**comb;reg[2]=num//denom
+        ip+=2
     return output
 
 part1=execute(prog,reg)
 
 def searchDigit(digitpos,start, stepsize):
-    if digitpos<0:
-        return start
+    if digitpos<0:return start
     for i in range(8):
         reg[0]=start+i*stepsize
         reg[1]=0
@@ -62,10 +37,9 @@ def searchDigit(digitpos,start, stepsize):
         output=execute(prog,reg)
         if output[digitpos]==prog[digitpos]:
             s=searchDigit(digitpos-1,start+i*stepsize,stepsize//8)
-            if s: return s
+            if s:return s
  
 start=8**(len(prog)-1)
 blocksize=8**(len(prog)-1)
 part2=searchDigit(len(prog)-1,start,blocksize)
-
-print(part1,part2)
+print("".join(map(str,part1)),part2)
