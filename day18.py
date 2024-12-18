263bb868b818754d43c82018324aa2db3d86c8d5
Donatien Bénéat <donatien_beneat@yahoo.fr>
2024-12-18 07:47:03 +0100

    4x faster with BFS instead of Dijkstra

--- day18.py
+++ day18.py
@@ -1,5 +1,4 @@
-from heapq import *
-from collections import defaultdict
+from collections import deque,defaultdict
 with open("data/input18.txt") as f:
     L=f.read().strip().split("\n")
 obst=defaultdict(lambda:999999)
@@ -13,31 +12,25 @@ def solveFor(k):
     seen = set()
     dist=defaultdict(lambda:999999)
     dist[(sx,sy)]=0
-    Q = []
-    for y in range(H):
-        for x in range(W):
-                if (x,y)==(sx,sy): heappush(Q,(0,(x,y)))
-                else: heappush(Q,(999999,(x,y)))
+    Q = deque()
+    Q.append((0,sx,sy))
     while Q:
-        d,u = heappop(Q)
-        x,y=u
+        d,x,y = Q.popleft()
         if (x,y)==(ex,ey):
+            dist[(x,y)]=d
             break
         if (x,y) in seen:
             continue
+        dist[(x,y)]=d
         seen.add((x,y))
         for xx,yy in [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]:
             if xx<0 or xx>=W or yy<0 or yy>=H: continue
             if obst[(xx,yy)]<=k: continue
             if (xx,yy) not in seen:
-                newdist = d+1
-                if newdist<=dist[(xx,yy)]:
-                    dist[(xx,yy)]=newdist
-                    heappush(Q,(newdist,(xx,yy)))
+                Q.append((d+1,xx,yy))
     return dist[(ex,ey)]
 
 part1=solveFor(1024)
-
 left,right=1024,len(L)-1
 while right-left>1:
     middle=(left+right)//2
@@ -46,5 +39,4 @@ while right-left>1:
     else:
         left=middle
 part2=L[right]
-
 print(part1,part2)
